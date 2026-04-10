9534c744f71d00132188a7ec28e13a804a8f173d
canetelizzi-rgb <canetelizzi@gmail.com>
2026-04-10 17:13:12 -0400

    iei_n1_c2

---
+++ datos_compuestos.py
@@ -0,0 +1,37 @@
+# Colecciones de DATOS
+
+# LISTAS => list
+# Es una colección ORDENADA y MUTABLE de datos de cualquier tipo
+
+print('Listas en Python')
+mi_primera_lista = ['elizabet cañete ',50,True]
+
+nombre_personal = input('Ingrese su nombre: ')
+print(type(mi_primera_lista))
+print(mi_primera_lista)
+print(f'El primer elemento de la lista es: {mi_primera_lista[0]}')
+mi_primera_lista[0] = nombre_personal
+print(mi_primera_lista)
+print(dir(mi_primera_lista))
+mi_primera_lista.append(25)
+print(mi_primera_lista)
+mi_primera_lista.remove(25)
+print(mi_primera_lista)
+
+# DICCIONARIOS dictionary => dict
+# Es una colección ORDENADA y MUTABLE de pares de datos de cualquier tipo
+# los datos de un diccionario ocupan el doble de espacio en memoria
+# deben almacenar la CLAVE y el VALOR de cada dato
+
+mi_primer_diccionario = {'nombre':'elizabet cañete','edad':50,'asistio a clase hoy?':True}
+print(type(mi_primer_diccionario))
+print(mi_primer_diccionario)
+
+print(mi_primer_diccionario["nombre"])
+mi_primer_diccionario['nombre'] = nombre_personal
+print( mi_primer_diccionario)
+mi_primer_diccionario['Esta feliz?']=True
+print(mi_primer_diccionario)
+print(dir(mi_primer_diccionario))
+# mi_primer_diccionario_modificado = mi_primer_diccionario.clear()
+# print(mi_primer_diccionario_modificado)
