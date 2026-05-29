62db09eb9252e3a921049a23fff7828c45465f80
canetelizzi-rgb <canetelizzi@gmail.com>
2026-05-29 12:14:28 -0400

    oh

---
+++ unidad_3/datos/usuarios.py
@@ -0,0 +1,93 @@
+listado_usuarios = [
+
+    ['nombre':'elizabet cagnete',
+     'telefono':'9 8888 8888',
+     'email':'hola_mundo@gmail.com',
+     'cantidad numeros':[
+         2,6,5,10
+         ],
+     'numero_rifa':'6',
+     ]
+
+    {'nombre':'juanita',
+     'telefono':'9 0000 0000',
+     'email':'holamundo2@gmail.com',
+     'cantidad numero':[
+         6,8,9,1,5
+         ]
+     'numero_rifa':'2',
+     }
+     
+     {'nombre':'juan',
+     'telefono':'9 1000 0000',
+     'email':'holamundo3@gmail.com',
+     'cantidad numero':[
+         6,8,9,1,5
+         ]
+     'numero_rifa':'2',
+     }
+
+     {'nombre':'francisco',
+     'telefono':'9 0003 0000',
+     'email':'holamundo4@gmail.com',
+     'cantidad numero':[
+         6,8,9,1,5
+         ]
+     'numero_rifa':'8',
+     }
+     
+     {'nombre':'panchito',
+     'telefono':'9 0000 0400',
+     'email':'holamundo5@gmail.com',
+     'cantidad numero':[
+         6,8,9,1,5
+         ]
+     'numero_rifa':'5',
+     }
+     
+     {'nombre':'maria',
+     'telefono':'9 0070 0000',
+     'email':'holamundo6@gmail.com',
+     'cantidad numero':[
+         6,8,9,1,5
+         ]
+     'numero_rifa':'1',
+     }
+     
+     {'nombre':'marcela',
+     'telefono':'9 0100 0000',
+     'email':'holamundo7@gmail.com',
+     'cantidad numero':[
+         6,8,9,1,5
+         ]
+     'numero_rifa':'4',
+     }
+     
+     {'nombre':'javiera',
+     'telefono':'9 0000 0900',
+     'email':'holamundo8@gmail.com',
+     'cantidad numero':[
+         6,8,9,1,5
+         ]
+     'numero_rifa':'3',
+     }
+     
+     {'nombre':'julieta',
+     'telefono':'9 0020 0000',
+     'email':'holamundo9@gmail.com',
+     'cantidad numero':[
+         6,8,9,1,5
+         ]
+     'numero_rifa':'7',
+     }
+     
+    {'nombre':'alejandra',
+     'telefono':'9 0008 0000',
+     'email':'holamundo10@gmail.com',
+     'cantidad numero':[
+         6,8,9,1,5
+         ]
+     'numero_rifa':'19',
+     }
+      
+]
